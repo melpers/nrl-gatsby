15aefa674d677a35d426468ec4e363be51e93ce2
Michael Elpers <melpers@zurka.com>
2019-11-08 13:02:42 -0500

    Update Gatsby and add markdown support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,12 @@
 import "./src/styles/index.scss"
 import "uswds_polyfills"
+
+// module.exports = {
+// 	onClientEntry: () => {
+// 		try {
+// 		  require('uswds_polyfills');
+// 		} catch (e) {
+// 		  // do nothing
+// 		}
+// 	}
+// };
