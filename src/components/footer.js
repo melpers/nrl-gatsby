672150cee584554270edf59a6b7d2b33d7e51565
Michael Elpers <melpers@zurka.com>
2019-11-11 11:34:23 -0500

    Add updated help icons. Adjust layout for new icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,39 +1,80 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import cx from 'classnames';
-// import { Footer as UswdsFooter } from 'uswds-react';
 import ExternalLink from '../components/externalLink';
+import SocialIcon from '../components/socialIcon';
 
-const Footer = ({ footer }) => (
-  <footer>
-    <div className="footer-wrapper">
-      <div className="footer-inner-wrapper">
-        <nav className="nrl-footer">
-          <div className="required-help-demo-block"></div>
-          <div className="social-demo-block"></div>
-          <div className="footer-primary-content-wrapper">
-            <p>This Is An Official U.S. Navy Website. U.S. Naval Research Lab 4555 Overlook Ave., SW Washington, DC 20375</p>
-            {footer.navigation.map((navGroup, idx) => (
-              <section key={idx} className="usa-footer-primary-content">
-                <ul className={ cx("usa-list", "usa-unstyled-list") }>
-                {navGroup.items.map((navItem, idx) => (
-                  <li key={idx} className="usa-nav-submenu-item">
-                    { navItem.type === 'internal' 
-                        ? 
-                      <Link to={navItem.link}>{navItem.text}</Link> 
-                        :
-                      ExternalLink(navItem)
-                    }
-                  </li>
-                ))}
-                </ul>
-              </section>
-            ))}
-          </div>
-        </nav>
+const Footer = ({ footer }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      socialIcons: allFile(
+        filter: {relativeDirectory: {eq: "social"}},
+        sort: {fields: name, order: ASC}
+      ) {
+        edges {
+          node {
+            name
+            publicURL
+            childImageSharp {
+              fluid(maxWidth: 32) {
+                  aspectRatio
+                  ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      },
+      helpIconSafe: file(
+        relativePath: {eq: "footer/safe-helpline.jpg"}
+      ) {
+        publicURL
+      },
+      helpIconCrisis: file(
+        relativePath: {eq: "footer/veteranscrisisline.jpg"}
+      ) {
+        publicURL
+      },
+    }
+  `)
+
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <div className="footer-inner-wrapper">
+          <nav className="nrl-footer">
+            <div className="social-block">
+              {data.socialIcons.edges.map((socialIcon, idx) => (
+                <SocialIcon key={socialIcon.node.name} data={socialIcon}></SocialIcon>
+              ))}
+            </div>
+            <div className="footer-primary-content-wrapper">
+              <p>This Is An Official U.S. Navy Website. U.S. Naval Research Lab 4555 Overlook Ave., SW Washington, DC 20375</p>
+              {footer.navigation.map((navGroup, idx) => (
+                <section key={idx} className="usa-footer-primary-content">
+                  <ul className={ cx("usa-list", "usa-unstyled-list") }>
+                  {navGroup.items.map((navItem, idx) => (
+                    <li key={idx} className="usa-nav-submenu-item">
+                      { navItem.type === 'internal' 
+                          ? 
+                        <Link to={navItem.link}>{navItem.text}</Link> 
+                          :
+                        ExternalLink(navItem)
+                      }
+                    </li>
+                  ))}
+                  </ul>
+                </section>
+              ))}
+            </div>
+          </nav>
+        </div>
+        <div className="help-block">
+          <a href="https://safehelpline.org/" target="_blank" rel="noopener noreferrer"><img src={data.helpIconSafe.publicURL} alt="D0D Safe Helpline 1-877-995-5247" /></a>
+          <a href="https://www.veteranscrisisline.net/" target="_blank" rel="noopener noreferrer"><img src={data.helpIconCrisis.publicURL} alt="Veterans Crisis Line 1-800-273-8255 Press 1" /></a>
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  )
+}
 
 export default Footer;
---
+++ src/components/socialIcon.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Img from 'gatsby-image'
+
+const SocialIcon = ({ data }) => {
+
+  const socialURLs = {
+    "facebook": "https://www.facebook.com/USNRL",
+    "linked-in": "http://www.linkedin.com/company/naval-research-laboratory",
+    "twitter": "https://twitter.com/usnrl/",
+    "youtube": "https://www.youtube.com/user/USNRL"
+  }
+
+  const height = 24;
+  const width = height * data.node.childImageSharp.fluid.aspectRatio;
+  
+  return (
+    <a href={socialURLs[data.node.name]} className="social-icon" target="_blank" rel="noopener noreferrer" style={{ width: width+"px" }}>
+        <Img 
+            fluid = {data.node.childImageSharp.fluid}
+            alt = {data.node.name}
+        />
+    </a>
+  )
+}
+  
+  export default SocialIcon;
+  
