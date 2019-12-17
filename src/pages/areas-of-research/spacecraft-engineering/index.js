c050d06d0c37bf5485ad385ad8a30aee368690a2
Michael Elpers <melpers@zurka.com>
2019-12-17 08:57:34 -0500

    Leadership layout changes

--- src/pages/areas-of-research/spacecraft-engineering/index.js
+++ src/pages/areas-of-research/spacecraft-engineering/index.js
@@ -160,20 +160,30 @@ const Index = (props) => {
           </div>
 
           <h2>Leadership</h2>
-          <div className="leadership-bio-block">
-            <Img fluid={data.profileImage.childImageSharp.fluid} />
-            <div className="leadership-bio-content">
-              <h3>GURPARTAP “GP” SANDHOO, D.SC.</h3>
-              <p className="bio-title">Superintendent: Spacecraft Engineering Department</p>
-              <p className="bio-contacts"><span className="bio-contact-label">E</span> <a href="mailto:spaenginfo@nrl.navy.mil">spaenginfo@nrl.navy.mil</a> <span className="bio-contact-label">P</span> (202) 767 6408 <span className="bio-contact-label">F</span> (202) 767 6429</p>
-            </div>
-          </div>
           <ExpandedText>
-            <p>Dr. Sandhoo is the Superintendent of the Spacecraft Engineering Division of the Naval Center of Space Technology (NCST) at the U.S. Naval Research Laboratory (NRL). As the superintendent, he provides executive direction and technical leadership in the development of policies and objectives necessary in conducting research, design and development in the areas of satellite and orbital transfer vehicle systems, with an emphasis on new and advanced space systems and technologies to improve the performance of the Navy mission.</p>
-            <p>The spacecraft engineering division is responsible for executing a $170 million program, including development of cutting-edge space capabilities in satellite servicing, space robotics, and satellite operations.</p>
-            <p>After working at NASA’s Johnson Space Center, Johns Hopkins University’s Applied Physics Laboratory, and in industry, Sandhoo began his career at NRL in 2005 as a senior aerospace engineer on the Operationally Responsive Space initiative. He progressed into broader roles at NRL, including time as the science advisor for the U.S. Navy’s Fifth Fleet in Bahrain, and as a member of Naval Warfare Integration group (N00X) on the staff of Chief of Naval Operations.</p>
-            <p>He holds a bachelor’s degree in mechanical engineering from the University of Maryland, a master’s degree in electrical engineering from Johns Hopkins University, a master’s from the U.S. Naval War College, a master’s and doctorate in aeronautics and astronautics from George Washington University, and is a MIT Seminar XXI fellow. Since 1986, he has served in uniform in the U.S. Marine Corps and U.S. Navy. Currently, Sandhoo is a Captain in the U.S. Navy Reserve as an Engineering Duty Officer.</p>
-            <p>He is recipient of numerous personal, unit, and campaign awards, which are all a tribute to the Sailors, Marines, and civilians he served alongside throughout his Navy, Marine and civilian career. Of them all, he most treasures his peer awarded Battalion Marine of the Year Award.</p>
+            <div className="leadership-bio-block">
+              <Img fluid={data.profileImage.childImageSharp.fluid} />
+              <div className="leadership-bio-content">
+                <h3>GURPARTAP “GP” SANDHOO, D.SC.</h3>
+                <p className="bio-title">Superintendent: Spacecraft Engineering Department</p>
+
+                <div className="bio-contact">
+                  <span className="bio-contact-label">E</span> <a href="mailto:spaenginfo@nrl.navy.mil">spaenginfo@nrl.navy.mil</a>
+                </div>
+                <div className="bio-contact">
+                  <span className="bio-contact-label">P</span> (202) 767 6408
+                </div>
+                <div className="bio-contact">
+                  <span className="bio-contact-label">F</span> (202) 767 6429
+                </div>
+
+              </div>
+              <p>Dr. Sandhoo is the Superintendent of the Spacecraft Engineering Division of the Naval Center of Space Technology (NCST) at the U.S. Naval Research Laboratory (NRL). As the superintendent, he provides executive direction and technical leadership in the development of policies and objectives necessary in conducting research, design and development in the areas of satellite and orbital transfer vehicle systems, with an emphasis on new and advanced space systems and technologies to improve the performance of the Navy mission.</p>
+              <p>The spacecraft engineering division is responsible for executing a $170 million program, including development of cutting-edge space capabilities in satellite servicing, space robotics, and satellite operations.</p>
+              <p>After working at NASA’s Johnson Space Center, Johns Hopkins University’s Applied Physics Laboratory, and in industry, Sandhoo began his career at NRL in 2005 as a senior aerospace engineer on the Operationally Responsive Space initiative. He progressed into broader roles at NRL, including time as the science advisor for the U.S. Navy’s Fifth Fleet in Bahrain, and as a member of Naval Warfare Integration group (N00X) on the staff of Chief of Naval Operations.</p>
+              <p>He holds a bachelor’s degree in mechanical engineering from the University of Maryland, a master’s degree in electrical engineering from Johns Hopkins University, a master’s from the U.S. Naval War College, a master’s and doctorate in aeronautics and astronautics from George Washington University, and is a MIT Seminar XXI fellow. Since 1986, he has served in uniform in the U.S. Marine Corps and U.S. Navy. Currently, Sandhoo is a Captain in the U.S. Navy Reserve as an Engineering Duty Officer.</p>
+              <p>He is recipient of numerous personal, unit, and campaign awards, which are all a tribute to the Sailors, Marines, and civilians he served alongside throughout his Navy, Marine and civilian career. Of them all, he most treasures his peer awarded Battalion Marine of the Year Award.</p>
+            </div>
           </ExpandedText>
         </div>
       </div>
