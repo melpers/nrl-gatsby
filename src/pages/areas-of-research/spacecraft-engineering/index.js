af44f1136b189c5e1b1baacaa95917980cdc52a8
Michael Elpers <melpers@zurka.com>
2019-12-17 07:32:33 -0500

    Img Polyfill test

--- src/pages/areas-of-research/spacecraft-engineering/index.js
+++ src/pages/areas-of-research/spacecraft-engineering/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from 'components/layout';
-import Img from "gatsby-image";
+import Img from "gatsby-image/withIEPolyfill";
 import HeroImage from 'components/heroImage';
 import Sidebar from 'components/sidebar';
 import Breadcrumbs from "components/breadcrumbs";
