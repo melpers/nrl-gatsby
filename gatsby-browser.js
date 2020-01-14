4724a32826445031a611e8a1c35961d93075958b
Michael Elpers <melpers@zurka.com>
2020-01-14 08:39:20 -0500

    Refactored for CJS vs. ES6 mixing issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,13 @@
+// See https://github.com/gatsbyjs/gatsby/issues/4718 for CJS vs. ES6 mixing issues
 import "./src/styles/index.scss"
-import "uswds_polyfills"
+import { wrapRootElement } from "./wrapRootElement"
 
-export { wrapRootElement } from './wrapRootElement'
+const onClientEntry = () => {
+    try {
+      require('uswds_polyfills');
+    } catch (e) {
+      // do nothing
+    }
+}
+
+export { wrapRootElement, onClientEntry }
