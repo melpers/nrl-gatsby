0004d4627b9fb60cdef9edf6dff9c972b5778096
Michael Elpers <melpers@zurka.com>
2020-03-31 14:18:53 -0400

    Debugging change

--- src/utils/clean-preview-uri.js
+++ src/utils/clean-preview-uri.js
@@ -5,15 +5,18 @@
 */
 const cleanPreviewUri = (uri) => {
     console.log(uri);
-    // remove trailing slash
-    uri = uri.replace(/\/$/, "");
-    console.log(uri);
+
     // if the uri starts with the preview string, remove the first 4 chunks
     if (uri.startsWith("/preview/")){
         uri = uri.replace(/([^/]*\/){5}/, '/')
     }
     console.log(uri);
-    return uri;
+
+    // remove trailing slash
+    uri = uri.replace(/\/$/, "");
+   console.log(uri);
+
+   return uri;
 };
  
  export { cleanPreviewUri };
