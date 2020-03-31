c7ee2025d2c5184d4fa2d448b822efc10f5fc705
Michael Elpers <melpers@zurka.com>
2020-03-31 14:24:31 -0400

    Remove debugging

--- src/utils/clean-preview-uri.js
+++ src/utils/clean-preview-uri.js
@@ -4,17 +4,13 @@
 *   Returns: /areas-of-research/spacecraft-engineering
 */
 const cleanPreviewUri = (uri) => {
-    console.log(uri);
-
     // if the uri starts with the preview string, remove the first 4 chunks
     if (uri.startsWith("/preview/")){
         uri = uri.replace(/([^/]*\/){5}/, '/')
     }
-    console.log(uri);
 
     // remove trailing slash
     uri = uri.replace(/\/$/, "");
-   console.log(uri);
 
    return uri;
 };
