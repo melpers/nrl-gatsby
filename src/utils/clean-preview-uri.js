dcf5f1c7652f688f5406dc7fc9d677811775424e
Michael Elpers <melpers@zurka.com>
2020-03-31 14:13:23 -0400

    Debugging change

--- src/utils/clean-preview-uri.js
+++ src/utils/clean-preview-uri.js
@@ -3,13 +3,16 @@
 *   Input: /preview/melpers/nrl-gatsby/v0.4/areas-of-research/spacecraft-engineering
 *   Returns: /areas-of-research/spacecraft-engineering
 */
-const cleanPreviewUri = uri => {
+const cleanPreviewUri = (uri) => {
+    console.log(uri);
     // remove trailing slash
     uri = uri.replace(/\/$/, "");
+    console.log(uri);
     // if the uri starts with the preview string, remove the first 4 chunks
     if (uri.startsWith("/preview/")){
         uri = uri.replace(/([^/]*\/){5}/, '/')
     }
+    console.log(uri);
     return uri;
 };
  
