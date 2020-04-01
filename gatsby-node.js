05486a0a7fba80263d3e9dae34be2d83a960358a
Michael Elpers <melpers@zurka.com>
2020-04-01 09:01:19 -0400

    Remove news release debugging

--- gatsby-node.js
+++ gatsby-node.js
@@ -195,7 +195,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
   `);
   newsResponse.data.allMarkdownRemark.edges.forEach(edge => {
     let slug = `${ edge.node.frontmatter.slug === null ? _.kebabCase(edge.node.frontmatter.title) : edge.node.frontmatter.slug }/`;
-    console.log("*** Slug: " + slug);
     createPage({
         component: newsTemplate,
         path: `/news/releases/${slug}`,
